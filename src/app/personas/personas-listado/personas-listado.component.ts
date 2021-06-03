8ccd1923d198f51e5cb88957033cae9f6ef8ac40
carlosdizx <61630633+carlosdizx@users.noreply.github.com>
2021-06-02 23:28:10 -0500

    Tablas responsivas

--- src/app/personas/personas-listado/personas-listado.component.ts
+++ src/app/personas/personas-listado/personas-listado.component.ts
@@ -1,35 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import {Persona} from '../persona';
-import {PersonaService} from '../persona.service';
-import {tap} from 'rxjs/operators';
-import {log} from 'util';
-import {PersonaSencilla} from '../PersonaSencilla';
+import { Persona } from '../persona';
+import { PersonaService } from '../persona.service';
+import { tap } from 'rxjs/operators';
+import { log } from 'util';
+import { PersonaSencilla } from '../PersonaSencilla';
 // import {PERSONAS} from '../personas.json';
 
 @Component({
-  selector: 'app-personas-listado',
-  templateUrl: './personas-listado.component.html',
-  styleUrls: []
+	selector: 'app-personas-listado',
+	templateUrl: './personas-listado.component.html',
+	styleUrls: [],
 })
 export class PersonasListadoComponent implements OnInit {
+	personas: PersonaSencilla[] = [];
 
-  personas: PersonaSencilla[] = [];
+	mostrar = true;
 
-  mostrar = true;
+	constructor(private service: PersonaService) {}
 
-  constructor(private service: PersonaService) {
-  }
+	ngOnInit(): void {
+		this.darListado();
+	}
 
-  ngOnInit(): void {
-     this.darListado();
-  }
+	darListado(): void {
+		this.service.getListado().subscribe((listado) => (this.personas = listado));
+	}
 
-  darListado(): void{
-    this.service.getListado().subscribe(listado => this.personas = listado);
-  }
-
-  cambiarEstado(): void{
-    this.mostrar = !this.mostrar;
-  }
-
-  }
+	cambiarEstado(): void {
+		this.mostrar = !this.mostrar;
+	}
+}
